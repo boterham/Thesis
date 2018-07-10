91149ef5eef630a568f96f10c653ffee882c9388
Martijn Witjes <martijnwitjes@gmail.com>
2018-07-10 20:39:36 +0200

    Update s0_master.r

--- s0_master.r
+++ s0_master.r
@@ -1,34 +1,33 @@
 rm(list = ls())
-# try(.libPaths("/home/bos053/R/lib/"))
+try(.libPaths("/home/bos053/R/lib/"))
 .libPaths()
 # Capacity Settings
-foldSize <- 200 # Size of each training fold for genetic optimization
+foldSize <- 1000 # Size of each training fold for genetic optimization
 pop.size <- 20
-max.trees <- 100
+max.trees <- 1000
 min.trees <- 1
-iterations <- 10 # Number of evolution iterations before a final model is chosen
-sampSize <- 500 # Size of each training fold for the final model
-cores <- 3
-
+iterations <- 100 # Number of evolution iterations before a final model is chosen
+sampSize <- 2000 # Size of each training fold for the final model
+cores <- 10
+getwd()
 
 filenames <- c("Scripts/s1_functions.R",
-               "Scripts/s2_preprocessing.r",
-               "Scripts/s3_optimization.r",
-               "Scripts/s4_validation.r",
-               "Scripts/s5_prediction.r",
-               "Scripts/s6_analysis.r")
-provinces <- data.frame(name=c("SonLa","LaiChau","DienBien","HoaBinh"),
-                        lastTrain=c(9,12,13,12),
-                        firstPred=c(10,13,14,13))
+               "Scripts/s2_preprocessing.R",
+               "Scripts/s3_optimization.R",
+               "Scripts/s4_validation.R",
+               "Scripts/s5_prediction.R",
+               "Scripts/s6_analysis.R")
+
+provinces <- data.frame(name=c("Test","SonLa","LaiChau","DienBien","HoaBinh"),
+                        lastTrain=c(9,9,12,13,12),
+                        firstPred=c(10,10,13,14,13))
+
 for (i in 1:nrow(provinces)){
-   dir <- paste0(getwd(),"/Provinces/",provinces$name[i],"/")
-   lastTrain <- provinces$lastTrain[i]
-   firstPred <- lastTrain + 1
-   try(sapply(filenames, source))
+  dir <- paste0(getwd(),"/Provinces/",provinces$name[i],"/")
+  lastTrain <- provinces$lastTrain[i]
+  firstPred <- lastTrain + 1
+  try(sapply(filenames, source))
 }
-
-
-
 
 prov.list <- list()
 for (i in 1:nrow(provinces)){
